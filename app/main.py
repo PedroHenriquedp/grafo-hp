793dd133c6419a8054064792987ad0956f32e833
pedrohenriquedp <pereira.henrique@aluno.ufabc.edu.br>
2023-11-07 00:03:25 -0300

    [FEATURE] - Adicionando scrape dos personagens e docstrings nas funções

--- app/main.py
+++ app/main.py
@@ -1,11 +1,46 @@
 import json
 
-with open("characters.json", encoding='utf-8') as personagem:
-    allchar = json.load(personagem)
+def read_personagem():
+    """Função para ler o arquivo de personagens da API https://hp-api.onrender.com/api/characters"""
+    with open("characters.json", encoding='utf-8') as personagem:
+        allchar = json.load(personagem)
+    return allchar
 
-contador = 0
-for personagem in allchar:
-    if personagem['actor'] != '':
-        personagemfic = personagem['name']
-        contador += 1
-        print(f'{personagemfic} {contador}')
+def create_dict_allchar():
+    """Função para fazer um scrape nos personagens e criar um dicionário próprio"""
+    allchar = read_personagem()
+
+    dic_personagem = []
+
+    for personagem in allchar:
+        thisdic_personagem = {
+            "nome": personagem["name"],
+            "especie": personagem["species"],
+            "genero": personagem["gender"],
+            "casa": personagem["house"],
+            "data_nascimento": personagem["dateOfBirth"],
+            "ano_nascimento": personagem["yearOfBirth"],
+            "mago": personagem["wizard"],
+            "ancestralidade": personagem["ancestry"],
+            "estudante_hogwarts": personagem["hogwartsStudent"],
+            "staff_hogwarts": personagem["hogwartsStaff"],
+            "ator_a": personagem["actor"],
+            "imagem": personagem["image"]
+        }
+        dic_personagem.append(thisdic_personagem)
+
+    return dic_personagem
+
+# def
+
+
+
+
+
+
+    # contador = 0
+    # for personagem in allchar:
+    #     if personagem['actor'] != '':
+    #         personagemfic = personagem['name']
+    #         contador += 1
+    #         print(f'{personagemfic} {contador}')
